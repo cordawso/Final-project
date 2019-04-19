eaceb9296ffb2a91c5e9cd2a62e114307b6db82a
Cory Dawson <cdd59@georgetown.edu>
2019-04-18 20:16:41 -0400

    Added reccomended plugins, some site content, and theme
    Added reccomended plugin like Jetpack, some of my portfolio content and  Blask theme and child theme

---
+++ themes/blask-child/functions.php
@@ -0,0 +1,21 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_styles() {
+ 
+    $parent_style = 'parent-style'; 
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+}
+// add custom menu to the theme
+function wpb_custom_new_menu() {
+  register_nav_menu('my-custom-menu',__( 'My Custom Menu' ));
+}
+add_action( 'init', 'wpb_custom_new_menu' );
+
+//remove the welcome panel. It's annoying
+
+remove_action('welcome_panel', 'wp_welcome_panel');
